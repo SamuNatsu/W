5948871a9dcb5f78c4cde13a5dbdafcdaf2055f2
SamuNatsu <49141130+SamuNatsu@users.noreply.github.com>
2021-04-17 16:20:04 +0800

    Update 404.php

--- 404.php
+++ 404.php
@@ -2,17 +2,25 @@
 <?php $this->need('header.php'); ?>
 
 <style>
-#header,#footer{
-  display: none;
-}
+#header,#footer{display: none;}
 .h404{
   font-size: 5rem;
   text-align: center;
-  margin: 200px 0;
+  margin: 100px 0 10px;
   opacity: 0.3;
+}
+a{
+  color: #000000;
+  opacity: 0.3;
+  transition: opacity .2s;
+}
+a:hover{
+  text-decoration: none;
+  opacity: 1.0;
 }
 </style>
 
-<h3 class="h404">えっと，ここは？<h3>
+<h3 class="h404"><b>4o4<br/>nOt FoUnD</b><h3>
+<p style="text-align: center;"><b><a href="<?php Helper::options()->siteUrl()?>">&gt;&gt; Back To Home &lt;&lt;</a></b></p>
 
 <?php $this->need('footer.php'); ?>
